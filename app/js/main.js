d5437e8b46315188eab033823f3ce0b7991b1ca8
aminaummabdulla <tminopt@gmail.com>
2020-11-13 00:49:00 +0200

    create adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,33 @@ $(function(){
           arrows: false,
           slidesToShow: 4,
           slidesToScroll: 4,
+          responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                 
+                }
+              },
+              {
+                breakpoint: 801,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                 
+                }
+              },
+          ]
 
     });
     $(".js-range-slider").ionRangeSlider({
@@ -32,6 +59,8 @@ $(function(){
         $('.icon-th-list').removeClass('active')
 
     });
+
+    $('input[type="file"], select').styler();
 
     $('.header__btn-menu').on('click', function(){
         $('.header__box').toggleClass('active');
